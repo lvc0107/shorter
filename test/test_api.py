7e7fe93a97007bb35772cd69c255265005e96d50
lvc0107 <lvc0107@protonmail.com>
2020-06-22 02:31:55 -0300

    Updated UT

--- test/test_api.py
+++ test/test_api.py
@@ -69,9 +69,18 @@ class TestURLApi:
             assert status_code == 401
         shorter_get_all_mock.assert_not_called()
 
-    def test_post_shorter_item(self, mocker, current_app_with_admin_user):
+    @pytest.mark.parametrize(
+        "url, given_code, expected_code",
+        [
+            ("http://url.com", "123456", "123456"),
+            ("http://url.com", None, "new_code"),
+        ],
+    )
+    def test_post_shorter_item(
+        self, mocker, current_app_with_admin_user, url, given_code, expected_code
+    ):
         api_mock = MagicMock()
-        api_mock.payload = dict(url="http://url.com", code="123456")
+        api_mock.payload = dict(url=url, code=given_code)
         url_api = Url(api=api_mock)
         validate_url_mock = mocker.patch.object(Validator, "validate_url")
         validate_url_mock.return_value = None, None
@@ -81,25 +90,41 @@ class TestURLApi:
             "shorter_app.apis.shorter.get_current_time",
             return_value=mocked_get_current_time,
         )
-        shorter_init_mock = mocker.patch.object(Shorter, "__init__", return_value=None)
-        stats_init_mock = mocker.patch.object(Stats, "__init__", return_value=None)
-
+        shorter_init_mock = mocker.patch("shorter_app.apis.shorter.Shorter")
+        shorter_init_mock.return_value = MagicMock(url=url, code=expected_code)
+        stats_init_mock = mocker.patch("shorter_app.apis.shorter.Stats")
+        stats_init_mock.return_value = MagicMock()
         shorter_repository_add_mock = mocker.patch.object(ShorterRepository, "add")
         stats_repository_add_mock = mocker.patch.object(StatsRepository, "add")
+        generate_shortcode_mock = mocker.patch(
+            "shorter_app.apis.shorter.generate_shortcode"
+        )
+        generate_shortcode_mock.return_value = expected_code
+
 
         response, status_code = url_api.post()
 
-        assert response.get("code") == None
+        assert response.get("code") == expected_code
+        assert response.get("url") == url
         assert 201 == status_code
-        shorter_init_mock.assert_called_once_with(code="123456", url="http://url.com")
+        shorter_init_mock.assert_called_once_with(url=url, code=expected_code)
 
         stats_init_mock.assert_called_once_with(
-            "123456", get_current_time_mock.return_value
+            expected_code, get_current_time_mock.return_value
         )
-        validate_url_mock.assert_called_once_with("http://url.com")
-        validate_code_mock.assert_called_once_with("123456")
-        stats_repository_add_mock.assert_called_once()
-        shorter_repository_add_mock.assert_called_once()
+        validate_url_mock.assert_called_once_with(url)
+        if given_code:
+            validate_code_mock.assert_called_once_with(given_code)
+            generate_shortcode_mock.assert_not_called()
+
+        else:
+            validate_code_mock.assert_not_called()
+            generate_shortcode_mock.assert_called_once_with()
+
+        shorter_repository_add_mock.assert_called_once_with(
+            shorter_init_mock.return_value
+        )
+        stats_repository_add_mock.assert_called_once_with(stats_init_mock.return_value)
 
     def test_post_invalid_code(self, mocker, current_app_with_admin_user):
         api_mock = MagicMock()
